a0b0f1c24a5c6892c1d8bc0bd280d21fbf043c92
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-07-01 14:29:00 -0400

    Also allow system property to override key

--- launcher-core/src/main/java/org/taumc/launcher/core/constants/APIKeys.java
+++ launcher-core/src/main/java/org/taumc/launcher/core/constants/APIKeys.java
@@ -4,14 +4,14 @@ package org.taumc.launcher.core.constants;
  * All API keys in this class should be changed if forking the launcher.
  */
 public class APIKeys {
-    public static final String CURSEFORGE = findKey("CURSEFORGE_CORE_API_KEY", "$2a$10$i6iAcI6XXzv53fKRksUk2u1Tz8zYPHOvD0xgPxnDB/ZKKdy73xFSi");
+    public static final String CURSEFORGE = findKey("CURSEFORGE_CORE_API_KEY", "taulauncher.curseforge.key", "$2a$10$i6iAcI6XXzv53fKRksUk2u1Tz8zYPHOvD0xgPxnDB/ZKKdy73xFSi");
 
-    private static String findKey(String envVar, String defaultKey) {
+    private static String findKey(String envVar, String sysProp, String defaultKey) {
         String key = System.getenv(envVar);
         if (key != null && !key.isBlank()) {
             return key;
         } else {
-            return defaultKey;
+            return System.getProperty(sysProp, defaultKey);
         }
     }
 }
