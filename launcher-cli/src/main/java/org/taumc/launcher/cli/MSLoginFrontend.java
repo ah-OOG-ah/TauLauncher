ff28516cff376b2ca4122a5d559ebec62a7c589a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-07-10 21:48:08 -0400

    Fix CLI

--- launcher-cli/src/main/java/org/taumc/launcher/cli/MSLoginFrontend.java
+++ launcher-cli/src/main/java/org/taumc/launcher/cli/MSLoginFrontend.java
@@ -3,9 +3,11 @@ package org.taumc.launcher.cli;
 import net.raphimc.minecraftauth.step.msa.StepMsaDeviceCode;
 import org.taumc.launcher.core.auth.microsoft.MicrosoftAccount;
 
+import java.util.concurrent.CompletableFuture;
+
 public class MSLoginFrontend implements MicrosoftAccount.LoginFlowFrontend {
     @Override
-    public void displayDeviceCode(StepMsaDeviceCode.MsaDeviceCode deviceCode) {
+    public void displayDeviceCode(StepMsaDeviceCode.MsaDeviceCode deviceCode, CompletableFuture<Void> future) {
         System.out.println("Navigate to the following URL: " + deviceCode.getDirectVerificationUri());
     }
 }
