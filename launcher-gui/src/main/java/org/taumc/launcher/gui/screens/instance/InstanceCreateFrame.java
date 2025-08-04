f521bceacba901edbf610abae449818cc502592a
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-04 10:43:19 -0500

    Add instance creation frame

---
+++ launcher-gui/src/main/java/org/taumc/launcher/gui/screens/instance/InstanceCreateFrame.java
@@ -0,0 +1,75 @@
+package org.taumc.launcher.gui.screens.instance;
+
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import com.formdev.flatlaf.extras.components.FlatTextField;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+public class InstanceCreateFrame extends JFrame {
+    private static final Border STD_BORDER = new LineBorder(Color.DARK_GRAY, 2, true);
+    private static final Border STD_MARGIN = new EmptyBorder(8, 8, 8, 8);
+    private static final Border HALF_MARGIN = new EmptyBorder(4, 4, 4, 4);
+    private static final Dimension STD_DIM_TEXTFIELD = new Dimension(320, 32);
+
+    public InstanceCreateFrame() {
+        var content = boxPanel(BoxLayout.Y_AXIS);
+        setContentPane(content);
+
+        content.add(createIconNameGroupPane(), BorderLayout.PAGE_START);
+    }
+
+    private static JPanel createIconNameGroupPane() {
+        var iconNameGroupPane = boxPanel(BoxLayout.X_AXIS);
+        iconNameGroupPane.setBorder(STD_BORDER);
+
+        var icon = new ChangeIconButton(new FlatSVGIcon("taulauncher/icons/poly/new.svg"));
+        iconNameGroupPane.add(wrapWithMargin(icon, STD_MARGIN));
+
+        iconNameGroupPane.add(createNameGroupPane());
+
+        return iconNameGroupPane;
+    }
+
+    private static JPanel createNameGroupPane() {
+        var nameGroupPane = boxPanel(BoxLayout.Y_AXIS);
+        nameGroupPane.setBorder(STD_MARGIN);
+
+        var nameSelect = new FlatTextField();
+        nameSelect.setLeadingComponent(new JLabel("Name: ", null, SwingConstants.LEFT));
+        nameSelect.setMinimumSize(STD_DIM_TEXTFIELD);
+        nameSelect.setPreferredSize(STD_DIM_TEXTFIELD);
+        nameGroupPane.add(wrapWithMargin(nameSelect, HALF_MARGIN));
+
+        var groupSelect = new FlatTextField();
+        groupSelect.setLeadingComponent(new JLabel("Group: ", null, SwingConstants.LEFT));
+        groupSelect.setMinimumSize(STD_DIM_TEXTFIELD);
+        groupSelect.setPreferredSize(STD_DIM_TEXTFIELD);
+        nameGroupPane.add(wrapWithMargin(groupSelect, HALF_MARGIN));
+
+        return nameGroupPane;
+    }
+
+    private static JPanel wrapWithMargin(JComponent comp, Border margin) {
+        var panel = boxPanel(BoxLayout.X_AXIS);
+        panel.add(comp);
+        panel.setBorder(margin);
+        return panel;
+    }
+
+    private static JPanel boxPanel(int axis) {
+        var panel = new JPanel();
+        //noinspection MagicConstant # intellij, this is *not a constant*
+        panel.setLayout(new BoxLayout(panel, axis));
+        return panel;
+    }
+}
