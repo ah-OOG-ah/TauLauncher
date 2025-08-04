c1cd10cac5ccec9aaa144fc51408d67e8fd96157
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-04 19:05:33 -0400

    Start adding flesh to vanilla config pane

--- launcher-gui/src/main/java/org/taumc/launcher/gui/screens/instance/creation/VanillaPane.java
+++ launcher-gui/src/main/java/org/taumc/launcher/gui/screens/instance/creation/VanillaPane.java
@@ -1,6 +1,23 @@
 package org.taumc.launcher.gui.screens.instance.creation;
 
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VanillaPane extends JPanel {
+    public VanillaPane() {
+        super();
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        var titlePane = Utils.boxPanel(BoxLayout.X_AXIS);
+        titlePane.setBorder(Utils.STD_MARGIN);
+
+        var title = new JLabel("Vanilla");
+        title.setMaximumSize(Utils.STD_DIM_MAX_TEXTFIELD);
+        titlePane.add(title);
+
+        titlePane.add(new JLabel(InstanceCreationModel.Type.VANILLA.icon));
+
+        add(titlePane);
+    }
 }
